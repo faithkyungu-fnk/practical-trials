3332121a6f5de1f1f3002684ebe99f54fd435885
faithkyungu-fnk <fnkyungu@gmail.com>
2026-07-06 04:07:16 +0300

    rest&spread

---
+++ rest.js
@@ -0,0 +1,4 @@
+showNumbers(10,20,30,40);
+function showNumbers(...numbers){
+    console.log(numbers);
+}
---
+++ spread.js
@@ -0,0 +1,5 @@
+const fruits=["apple", "banana","cherries"]
+console.log(...fruits);
+const first=["Faith", "Ocean"]
+const second=["Skye","Winter"]
+console.log(...first,...second)
